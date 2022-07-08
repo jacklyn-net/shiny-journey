f621ba80e28a96fcdecb297c14512700acd0f178
jacklyn-net <jacklyn@netlify.com>
2022-07-08 13:55:36 -0500

    Installed Material, added Snackbar component

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,54 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home({ images }, copyUrl) {
+import * as React from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
-  // console.log('halp', images)
+export default function Home({ images }) {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
 
   return (
     <div className="container">
       <Head>
         <title>ʕ•ᴥ•ʔ</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Pacifico"></link>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans"></link>
       </Head>
       <main>
-        <Header title="cute things are here" />
+        <Header title="[ cute things here ]" />
         <div className="text">
-          When you click a photo, an image markdown code is copied to your <br></br>clipboard so you can easily use the image in your GitHub issues or PRs.
+          When you click a photo, an image markdown code is copied to your<br></br>clipboard so you can easily use the image in your GitHub issues or PRs.
+          <br></br><br></br>
+          all images from <a href="https://unsplash.com/">unsplash</a>
         </div>
         <div className="all_images">
         {images.map((image ) => (
@@ -23,12 +57,24 @@ export default function Home({ images }, copyUrl) {
           <img
             src={image.url}
             alt={image.alt}
-            onClick={() => {navigator.clipboard.writeText(image.copyMe)}}
+            onClick={() => {navigator.clipboard.writeText(image.copyMe) && handleClick()}}
+          />
+          <Snackbar
+            open={open}
+            autoHideDuration={2200}
+            onClose={handleClose}
+            message="✅  &nbsp; Copied!"
+            action={action}
+            ContentProps={{
+              sx: {
+                background: "#1D2F6F",
+                boxShadow: 0,
+              }
+            }}
           />
           </div>
         ))}
         </div>
-
       </main>
       <Footer />
     </div>
@@ -46,8 +92,6 @@ export async function getStaticProps() {
   }).then(r => r.json());
 
   const { resources } = results;
-
-  // console.log(results);
 
   const images = resources.map(resource => { 
     return {
